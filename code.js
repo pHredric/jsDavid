f6f53525436360ecaaf1d7a79a99e0ff183f65ef
phredric <fleriksson87@gmail.com>
2018-03-13 14:27:45 +0100

    new init

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-function RenderData(movie)
+function OldRenderData(movie)
 {
     let image = document.getElementById('movieImage');
     image.setAttribute("src", movie.imgSrc);
@@ -17,4 +17,72 @@ function RenderData(movie)
     actorlistholder.innerHTML = actorString;
 }
 
+function OldChangeStarRating(grade)
+{
+    console.log("grade är: "+grade);
+    for (let index = 1; index <= 5; index++) {
+        let star = document.getElementById("star"+index);
+
+        console.log("starX är: "+star.getAttribute("id"));
+
+        if(index <= grade)
+        {
+            star.setAttribute("class", "filledStars");
+        }
+        else
+        {
+            star.setAttribute("class", "emptyStars");
+        }
+    }
+}
+
+function RenderData(movie)
+{
+    $('#movieImage').attr("src", movie.imgSrc);
+    $('#movieTitle').text(movie.title);
+    $('#movieDescription').text(movie.description);
+    movie.actors.forEach(actorname => {
+        $('#movieActors').append("<li>"+actorname+"</li>");;
+    });
+};
+
+function ChangeStarRating(grade)
+{
+    console.log("grade är: "+grade);
+    //console.log($("#star"+grade).attr('id'))
+    for (let index = 1; index <= 5; index++) {
+        if(index <= grade)
+        {
+            $("#star"+index).addClass("filledStars");
+        }
+        else
+        {
+            $("#star"+index).removeClass("filledStars");
+        }
+    }
+}
+
 RenderData(movieData);
+ChangeStarRating(2);
+
+// document.getElementById("star1").addEventListener("click", function(){
+//     ChangeStarRating(1);
+// });
+// document.getElementById("star2").addEventListener("click", function(){
+//     ChangeStarRating(2);
+// });
+// document.getElementById("star3").addEventListener("click", function(){
+//     ChangeStarRating(3);
+// });
+// document.getElementById("star4").addEventListener("click", function(){
+//     ChangeStarRating(4);
+// });
+// document.getElementById("star5").addEventListener("click", function(){
+//     ChangeStarRating(5);
+// });
+
+for (let index = 1; index <= 5; index++) {
+    $("#star"+index).click(function() {
+        ChangeStarRating(index);
+    })
+}
--- data.js
+++ data.js
@@ -3,6 +3,6 @@ let movieData = {
     imgSrc: "http://www.spelochfilm.se/wp-content/uploads/2017/03/inception2-678x381.jpg",
     description: `A very meta movie about dreams within dreams. And then there was
     a whole bunch of shooting, not really sure how that fit into the picture`,
-    actors: ["Leonardo DiCaprio", "Ellen Page", "Ken Watanabe"]
+    actors: ["Leonardo DiCaprioA", "Ellen Page", "Ken Watanabe"]
 };
 
